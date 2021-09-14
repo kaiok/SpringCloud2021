30c29ac2bf9688259bb3cad4738314117fbce618
Kais_C <Kais_215@outlook.com>
2021-09-14 13:40:57 +0800

    修改Ribbon轮询算法为随机

--- cloud-consumer-order80/src/main/java/com/kais/springcloud/Controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/kais/springcloud/Controller/OrderController.java
@@ -3,6 +3,7 @@ package com.kais.springcloud.Controller;
 import com.kais.springcloud.entities.CommonResult;
 import com.kais.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,11 @@ public class OrderController {
     //单机版，服务访问IP地址写死/
     // private static final String PAYMENT_SRV_URL = "http://localhost:8001";
 
-    // 通过在eureka上注册过的微服务名称调用
-    // 集群联机版
+    /**
+     * 方法描述:通过在eureka上注册过的微服务名称调用
+     * 集群联机版
+     *
+    */
     private static final String PAYMENT_SRV = "http://CLOUD-PAYMENT-SERVICE";
 
     @Autowired
@@ -33,6 +37,8 @@ public class OrderController {
      * 方法描述:创建order订单
      * 客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
      * (url, requestMap, ResponseBean.class)这三个参数分别代表：REST请求地址、请求参数、HTTP响应转换被转换成的对象类型
+     * postForObject：返回对象为响应体中数据转化成的对象，基本上可以理解为Json
+     * getForEntity：返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等
      *
  * @param payment
      * @返回值 : com.kais.springcloud.entities.CommonResult
@@ -48,6 +54,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/get{id}")
     public CommonResult getPayment(@PathVariable Long id){
         return restTemplate.getForObject(PAYMENT_SRV + "/payment/get/" + id,CommonResult.class);
+    }
+
+    @GetMapping("consumer/payment/getForEntity/{id")
+    public CommonResult getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_SRV + "/payment/get" + id,CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
     }
 
 }
--- cloud-consumer-order80/src/main/java/com/kais/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/kais/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.kais.springcloud;
 
+import com.kais.springcloud.myRule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Author liuxiankai
@@ -12,6 +14,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+/**
+ * 方法描述:将ribbon的负载均衡算法有轮询改为随机
+*/
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
